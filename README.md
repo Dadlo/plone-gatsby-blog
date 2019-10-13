cb043b8d29d62e74cd3955bd3844c71bcb371f11
Danilo Sartorelli Barbato <danilo.barbato@Danilos-MacBook-Pro.local>
2019-10-13 22:50:42 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Plone Gatsby Blog
+
+Created with gatsby-starter-plone
+
+## Develop
+
+```
+
+Run develop:
+
+```sh
+cd plone-gatsby-blog
+gatsby develop
+```
+
+## Build static
+
+```
+
+Run build:
+
+```sh
+cd plone-gatsby-blog
+gatsby build
+```
---
+++ gatsby-config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Kickstart Gatsby static site development with Plone',
+    subTitle: 'Plone + Gatsby = \u2764',
+  },
+  pathPrefix: '/',
+  plugins: [
+    {
+      resolve: 'gatsby-source-plone',
+      options: {
+        baseUrl: 'http://localhost:8080/Plone/',
+        logLevel: 'DEBUG',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/static`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Gatsby Starter Plone',
+        short_name: 'Plone',
+        start_url: '/',
+        background_color: '#ffffff',
+        theme_color: '#007eb6',
+        display: 'standalone',
+        icon: 'src/images/icon.png',
+    },
+  },
+    'gatsby-plugin-offline',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+  ],
+};
