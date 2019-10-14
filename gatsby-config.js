63a9c85f348bd73906d0e6278f81a40a9ae02d33
Danilo Sartorelli Barbato <dadlobr@gmail.com>
2019-10-14 23:44:37 +0200

    Add commented config for favicon plugin for later reference

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,6 +71,24 @@ module.exports = {
         ],
       },
     },
+    /*{
+      resolve: 'gatsby-plugin-favicon',
+      options: {
+        logo: `./src/images/icon.png`,
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: true
+        }
+      }
+    },*/
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
