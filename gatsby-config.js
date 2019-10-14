cf494aeb586c3a31ef299f326ece7816058ad8dd
Danilo Sartorelli Barbato <dadlobr@gmail.com>
2019-10-14 21:44:17 +0200

    Update API path

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-plone',
       options: {
-        baseUrl: 'http://839d6ec4.ngrok.io/Plone/',
+        baseUrl: 'http://b856bc3a.ngrok.io/Plone/',
         logLevel: 'DEBUG',
       },
     },
